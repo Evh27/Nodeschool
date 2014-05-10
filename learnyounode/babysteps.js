fb37d32f616eafdfb2ecfa9f7a9d887529377806
Richard Goater <richard.goater27@gmail.com>
2014-05-10 18:01:00 +0100

    jshint corrections

--- learnyounode/babysteps.js
+++ learnyounode/babysteps.js
@@ -3,7 +3,7 @@ var args = process.argv.slice(2);
 var sum = Number(0);
 
 args.forEach(function(arg) {
-  sum += Number(arg)
+  sum += Number(arg);
 });
 
 console.log(sum);
--- learnyounode/myfirstio.js
+++ learnyounode/myfirstio.js
@@ -2,4 +2,4 @@ var fs = require('fs');
 
 var buffer = fs.readFileSync(process.argv[2]);
 
-console.log(buffer.toString().split('\n').length - 1)
+console.log(buffer.toString().split('\n').length - 1);
