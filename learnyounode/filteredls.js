ca410eaf3fa18c4bfe47ab3179949c86d1d4ed7f
Richard Goater <richard.goater27@gmail.com>
2014-05-10 19:08:26 +0100

    Amended extension match condition

--- learnyounode/filteredls.js
+++ learnyounode/filteredls.js
@@ -5,7 +5,7 @@ var args = process.argv.slice(2);
 
 fs.readdir(args[0], function(err, list) {
   list.filter(function(filename) {
-    return path.extname(filename).indexOf(args[1]) !== -1;
+    return path.extname(filename).slice(1) === args[1];
   })
   .forEach(function(file) {
     console.log(file);
