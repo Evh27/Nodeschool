4dd27a4203427feb6b34131f3f20d56709525ac4
Richard Goater <richard.goater27@gmail.com>
2014-06-02 20:28:12 +0100

    Explanation for Function Call added.

--- functional-javascript-workshop/function-call.js
+++ functional-javascript-workshop/function-call.js
@@ -1,1 +1,13 @@
 module.exports = Function.call.bind(Array.prototype.slice);
+
+// ok! I looked this one up - but I can explain it:
+//
+// bind() returns a function like so:
+//
+//   function( /* args */ ) {
+//     return Array.prototype.slice.call( /* args */ )
+//   }
+//
+// and because call's first argument will be the value of 'this',
+// using the bound function as slice(array, number, number) will
+// work as specified, nice really.
